51c09796962e4950ebf0fa6f8b42eaec0a339518
Tinotenda Musikavanhu <musikavanhu1@gmail.com>
2022-03-19 10:53:33 -0500

    Delete HealthEat-main 2 directory

--- HealthEat-main 2/models/index.js
+++
@@ -1,8 +0,0 @@
-// export objects containing models
-// each of these is a different db- combine menu/recipe?
-
-module.exports = {
-    // Menu: require("./menu"),
-    // Recipe: require("./recipe"),
-    User: require("./user"),
-}
